48845b8061c0d8d4f9daa71feca6bf0a29c0551c
Taleh <t.mirzeyev@gmail.com>
2022-02-20 13:20:22 +0300

    finish

---
+++ 34/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 34: Написать программу для замены элементов массива на противоположные.
+int[] massiv=new int [15];
+void printArray(int []printed){
+    for(int i =0;i<printed.Length;i++){
+        Console.Write($"({printed[i]}) ");
+    }
+}
+void fillArray(int [] filled){
+    for(int i=0;i<filled.Length;i++){
+        filled[i]= new Random().Next(1,15);
+    }
+}
+
+void opposite(int[] inverse){
+    for(int i=0;i<inverse.Length;i++){
+        Console.Write($"({inverse[i] * -1 })");
+    }
+}
+fillArray(massiv);
+printArray(massiv);
+Console.WriteLine();
+opposite(massiv);
