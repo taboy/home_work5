033f30ea6328f4fd3ff1dc01bfd102e84743253f
Taleh <t.mirzeyev@gmail.com>
2022-02-20 13:34:06 +0300

    finish

---
+++ 36/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечётных/чётных чисел.
+int[] massiv=new int [15];
+void printArray(int []printed){
+    for(int i =0;i<printed.Length;i++){
+        Console.Write($"({printed[i]}) ");
+    }
+}
+void fillArray(int [] filled){
+    for(int i=0;i<filled.Length;i++){
+        filled[i]= new Random().Next(10,99);
+    }
+}
+void even(int[] even){
+    int evenc=0;
+    int nonevenc=0;
+    for(int i =0;i<even.Length;i++){       
+        if (even[i]%2==0) evenc++;
+        else nonevenc ++;       
+    }
+    Console.WriteLine($"count of even numbers is {evenc} count of noneven number is {nonevenc}");
+}
+fillArray(massiv);
+printArray(massiv);
+even(massiv);
